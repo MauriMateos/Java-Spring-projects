87e23066a831a405cfd562c003cb20c321a9f663
MauriMateos <mauriciomicaelmateos@gmail.com>
2023-12-19 11:11:45 -0300

    Project init

---
+++ gallon-to-liter/src/test/java/com/unitconverter/gallontoliter/GallonToLiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unitconverter.gallontoliter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GallonToLiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
